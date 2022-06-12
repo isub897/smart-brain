20ff82def64594754afab598f4885cf1e2a8e306
isub897 <ivsubritzky@gmail.com>
2022-06-12 21:10:41 +1200

    added image url form component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from "react";
 import brain from './brain.png'
 import Tilt from 'react-tilt';
+import './Logo.css';
 
 const Logo = () => {
     return(
         <div className="Logo">
-            <Tilt className="Til ma3" options={{ max : 50 }} style={{ height: 100, width: 100 }} >
-                <div className="Tilt-inner pa2 pointer">
+            <Tilt className="Til ma3 shadow-5 br3" options={{ max : 50 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pv4 br3 pointer">
                     <img alt="logo" src={brain} /> 
                 </div>
             </Tilt>
